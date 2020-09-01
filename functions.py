39df2ec3ac15eff213e0070312bd195bfa6cfbec
Rafa <rafavazquez229@gmail.com>
2020-09-01 12:44:33 -0700

    Finished

---
+++ functions.py
@@ -0,0 +1,28 @@
+def area(width, height):
+    result = width * height
+    return result
+result1 = area(5, 6)
+result2 = area(10, 10)
+result3 =  area(2, 2)
+print(result1)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+sum1 = subtract(3, 1)
+sum2 = subtract(2, 5)
+sum3 = subtract(4, 2)
+print(sum1)
+print(sum2)
+print(sum3)
+
+#"Write a function called add which takes in two numbers and returns the numbers summed together"
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+add1 = add(1, 1)
+add2 = add(2, 2)
+print(add1)
+print(add2)
